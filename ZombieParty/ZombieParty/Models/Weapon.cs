bf25edf0c90f1061898f5a274af2362953b7760b
Kostiantyn Bohachov <2397099@cegepmontpetit.ca>
2026-09-01 10:59:28 -0400

    FCT Création d'une classe Weapon
    Ajout le menu déroulant Armory et l’action Manage Weapons

---
+++ ZombieParty/ZombieParty/Models/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZombieParty.Models
+{
+    public class Weapon
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Force { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Image { get; set; }
+        public int Qty { get; set; }
+        public int QtyBought { get; set; }
+    }
+
+}
